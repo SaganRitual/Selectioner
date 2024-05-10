4bf05d357aa87b92d579c9bb981c37adcb831c0d
Rob Bishop <c.sagan.ritual@gmail.com>
2024-05-09 17:20:58 -0700

    Initial Commit

---
+++ Selectioner/ContentView.swift
@@ -0,0 +1,19 @@
+// We are a way for the cosmos to know itself. -- C. Sagan
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Selectioner/SelectionerApp.swift
@@ -0,0 +1,12 @@
+// We are a way for the cosmos to know itself. -- C. Sagan
+
+import SwiftUI
+
+@main
+struct SelectionerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
